9c6fd7be559930c770b3244d759ecc3227fbdc08
udaybk <uday.nbausj@gmail.com>
2021-01-05 20:56:48 +0530

    added status code

--- health/views.py
+++ health/views.py
@@ -1,9 +1,10 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 
 @api_view(['GET'])
 def health(req):
     return Response(data={
         "msg": "everything is alright"
-    })
+    }, status=status.HTTP_200_OK)
